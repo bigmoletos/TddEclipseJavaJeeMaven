f479680d5cd1f9f4cf3f8dbf25ee7638961eef16
bigmoletos <bigmoletos@yopmail.com>
2018-11-09 17:46:58 +0100

    modif repertoires

---
+++ exemple-tdd/src/main/java/fr/demandeatonton/tdd/exceptions/InvalidListException.java
@@ -0,0 +1,13 @@
+package fr.demandeatonton.tdd.exceptions;
+
+@SuppressWarnings("serial")
+public class InvalidListException extends RuntimeException {
+
+	/**
+	 * @param arg0
+	 */
+	public InvalidListException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+}
